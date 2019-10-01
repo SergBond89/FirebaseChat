8e13407044f1e6ad51b1e0a4eb0aaadd411eb0ea
Sergey Bondar <sergdevbond@gmail.com>
2019-10-01 14:14:34 +0300

    Initial commit

---
+++ FirebaseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Sergey on 10/1/19.
+//  Copyright © 2019 SergBondCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
